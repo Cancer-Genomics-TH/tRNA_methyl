b3e5faef94cdcaf0fbb205113df14840d8d7e90c
Darek Kedra <darek.kedra@gmail.com>
2022-05-24 00:45:23 +0200

    batch_clumpify.py refactor textwrap.dedent, functions

--- src/batch_clumpify.py
+++ src/batch_clumpify.py
@@ -7,71 +7,98 @@ runs clumpify from BBmap to reduce the size
 import glob
 import os
 import sys
-
-input_dir = "./ORIG/"
-fastq_pattern = "*.fastq.gz"
-
-output_dir_top = "./"
-out_dir = f"{output_dir_top}/CLUMP_20220426"
-
-clumpify_exe = "/scratch/dkedra/soft/progs/bbmap_current/clumpify.sh"
-num_of_threads = 16
-num_pigz_threads = 6
+import textwrap
 
 
-shell_file_header = f"""#!/bin/bash
+def slurm_setup(job_name):
+    """prints the SLURM shell file header"""
 
-#SBATCH --nodes=1
-#SBATCH --time=01:00:00
-#SBATCH --cpus-per-task={num_of_threads}
-#SBATCH --partition=mem
-"""
+    shell_file_header = f"""
+    #!/bin/bash"
 
-env_setup = """
-# module load gcc/11.2.0
-module load java/11
+    #SBATCH --nodes=1 
+    #SBATCH --time={JOB_TIME} 
+    #SBATCH --cpus-per-task={NUM_OF_THREADS}
+    #SBATCH --partition={SLURM_PARTITION}
 
-export PATH=$HOME/soft/bin:$PATH
-"""
+    """
+    shell_file_header = textwrap.dedent(shell_file_header)
+
+    env_setup = """
+    module load java/11
+    # pigz install dir
+    export PATH=$HOME/soft/bin:$PATH
+    """
+
+    env_setup = textwrap.dedent(env_setup)
+
+    print(shell_file_header)
+    print(f"#SBATCH --job-name={job_name}")
+    print(env_setup)
 
 
-for fastq_fn in glob.glob(f"{input_dir}/{fastq_pattern}"):
+def one_file_shell(fastq_fn):
     # save the basename  and ending
     fastq_fn_base = fastq_fn.split("/")[-1]
     fastq_fn_base = fastq_fn_base.replace(".fastq.gz", "")
 
-    out_fq_fn = f"{out_dir}/{fastq_fn_base}.clump_opt_dedup.fq.gz"
+    out_fq_fn = f"{TOP_OUT_DIR}/{fastq_fn_base}.clump_opt_dedup.fq.gz"
 
     job_name = f"clump_{fastq_fn_base}"
     shell_fn = f"{job_name}.sh"
 
     saveout = sys.stdout
-    output_fh = open(shell_fn, "w")
-    sys.stdout = output_fh
 
-    print(shell_file_header)
-    print(f"#SBATCH --job-name=job_name")
-    print(env_setup)
+    with open(shell_fn, "w", encoding="utf-8") as output_fh:
+        sys.stdout = output_fh
+        slurm_setup(job_name)
 
-    command_1 = f"""{clumpify_exe} \\
+        print("""echo "starting clumpify" """)
+        print(date)
+
+        command_1 = f"""
+        {clumpify_exe} \\
         dedupe=t \\
         optical=t \\
         reorder=a \\
         shortname=shrink \\
         blocksize=2048 \\
         ziplevel=8 \\
-        pigz={num_pigz_threads} \\
-        unpigz={num_pigz_threads} \\
+        pigz={NUM_PIGZ_THREADS} \\
+        unpigz={NUM_PIGZ_THREADS} \\
         lowcomplexity=t \\
         in={fastq_fn} \\
         out={out_fq_fn}
-    """
+        """
 
-    command_1 = command_1.replace(8 * " ", "")
+        command_1 = textwrap.dedent(command_1)  # .replace(8 * " ", "")
 
-    print(command_1)
-    print("\n\n")
+        print(command_1)
 
-    sys.stdout = saveout
-    output_fh.close()
-    os.system("chmod +x %s" % (shell_fn))
+        print("\n\n")
+        print("""echo "finished clumpify" """)
+        print(date)
+
+        sys.stdout = saveout
+        output_fh.close()
+        os.system("chmod +x %s" % (shell_fn))
+
+
+if __name__ == "__main__":
+    ## setup clump
+    INPUT_DIR = "./ORIG/"
+    INPUT_PATTERN = "*.fastq.gz"
+
+    TOP_OUT_DIR = "./"
+    CLUMP_OUT_DIR = "CLUMP_20220524"
+    output_dir_top = f"{TOP_OUT_DIR}/CLUMP_OUT_DIR"
+
+    clumpify_exe = "/scratch/dkedra/soft/progs/bbmap_current/clumpify.sh"
+    NUM_OF_THREADS = 16
+    NUM_PIGZ_THREADS = 6
+
+    # setup
+    JOB_TIME = "05:00:00"
+    SLURM_PARTITION = "mem"
+    for fastq_fn in glob.glob(f"{INPUT_DIR}/{INPUT_PATTERN}"):
+        one_file_shell(fastq_fn)
