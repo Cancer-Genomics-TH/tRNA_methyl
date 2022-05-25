ba9b710dfeb02fc0a8a0b3fd42742cbe5fc2bed0
Darek Kedra <darek.kedra@gmail.com>
2022-05-25 13:25:39 +0200

    refactor parse_fqgrep_report.py using pylint hints

--- src/parse_fqgrep_report.py
+++ src/parse_fqgrep_report.py
@@ -1,12 +1,24 @@
-#!/scratch/dkedra/soft/progs/pypy3.9_current/bin/pypy3
+#!/usr/bin/env pypy3
+
+"""
+creates a fastq file out of a fqgrep report
+
+input: gzipped fqgrep report
+output: fastq fn printed on the STDOUT
+
+usage:
+
+parse_fqgrep_report.py some_report.gz > output.fastq
+
+"""
+
 
 import gzip
 import sys
 
-input_fn = sys.argv[1]
-
 
 def parse_list(fqgrep_columns):
+    """transform a list from one report row into fastq for one read"""
     read_name = fqgrep_columns[0]
     read_seq = fqgrep_columns[-2]
     read_qual = fqgrep_columns[-1]
@@ -15,31 +27,37 @@ def parse_list(fqgrep_columns):
     if match == "*":
         out_seq = read_seq
         out_qual = read_qual
-        # print(f"no_match: {fqgrep_columns}")
+        # debug: print(f"no_match: {fqgrep_columns}")
     else:
-        start = int(sl[-5])
-        end = int(sl[-4])
+        start = int(fqgrep_columns[-5])
+        end = int(fqgrep_columns[-4])
         # print(start, end)
         seq_mask = "N" * (end - start)
         qual_mask = "B" * (end - start)
         out_seq = f"{read_seq[:start]}{seq_mask}{read_seq[end:]}"
         out_qual = f"{read_qual[:start]}{qual_mask}{read_qual[end:]}"
-        # print(out_seq)
-        # print(f"got_match: {fqgrep_columns}")
+        # debug: print(out_seq)
+        # debug: print(f"got_match: {fqgrep_columns}")
     print(f"@{read_name}")
     print(out_seq)
     print("+")
     print(out_qual)
 
 
-with gzip.open(input_fn, mode="rt") as fh:
-    counter = 0
-    for line in fh:
-        if counter == 0:
-            pass
-        else:
-            line = line.strip()
-            sl = line.split()
-            parse_list(sl)
-            # print(sl)
-        counter += 1
+def report_to_fastq(input_fn):
+    """parse the gzipped fqgrep report to masked fastq"""
+    with gzip.open(input_fn, mode="rt") as fh:
+        counter = 0
+        for line in fh:
+            if counter == 0:
+                pass
+            else:
+                line = line.strip()
+                sl = line.split()
+                parse_list(sl)
+            counter += 1
+
+
+if __name__ == "__main__":
+    input_report_fn = sys.argv[1]
+    report_to_fastq(input_report_fn)
