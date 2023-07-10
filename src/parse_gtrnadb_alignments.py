57ad33dcffd248c04dc8ad2423be57ab29433a96
Darek Kedra <darek.kedra@gmail.com>
2023-07-10 17:34:21 +0200

    extract aligments from gtRNAdb andd save as Stockholm script

---
+++ src/parse_gtrnadb_alignments.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+# coding: utf-8
+# # Notebook to parse tRNA alignments from HTML page gtRNAdb
+# 
+# working version
+# requires manual editing/removing of a duplicate ```RZ9990_NCA_HUMAN``` entry at the end of the file
+# output: hs38_gtrnadb_algn_01.sto
+
+
+
+
+
+import requests
+import sys
+
+from bs4 import BeautifulSoup
+
+
+
+
+url = "http://gtrnadb.ucsc.edu/genomes/eukaryota/Hsapi38/Hsapi38-align.html"
+
+out_stockholm_fn = "hs38_gtrnadb_algn_01.sto"
+
+align_header = "# STOCKHOLM 1.0"
+
+
+
+
+def parse_seq_alignments(alg_list):
+    counter = 0
+    old_id_prefix = ""
+    old_anticodon = ""
+    for alig in alg_list:
+        lines = alig.text.split("\n")
+        for line in lines:
+            if line.find(">>") == -1 and line.find("filtered") == -1:
+                sl = line.split()
+                if len(sl) != 0:
+                    #print(line)
+                    seq, seq_id = sl[:2]
+                    if seq_id.startswith("tRNA"):
+                        split_id = seq_id.split("-")
+                        id_prefix = "-".join(split_id[:3])
+                        try:
+                            anticodon = split_id[2]
+                        except:
+                            break
+                            # there is an error in the HTML before the tRNA_Sup
+
+                        if id_prefix != old_id_prefix:
+                            if counter != 0:
+                                print("//")
+                            counter += 1
+                            print(align_header)
+                            align_group_id = f"#=GF ID {id_prefix}"
+                            print(align_group_id)
+
+                        out_str = f"{seq_id}\t{seq}"
+                        print(out_str)
+                        old_id_prefix = id_prefix
+                    else:
+                        split_id = seq_id.split("_")
+                        anticodon = split_id[1]
+                        short_id = f"{split_id[0]}_{split_id[1]}_HUMAN"
+                        out_str = f"{short_id}\t{seq}"
+                        print(out_str)
+
+    print("//")    
+
+def output_stockholm(alig_list):
+
+    saveout = sys.stdout
+    output_fh = open(out_stockholm_fn, "w")
+    sys.stdout = output_fh
+
+    parse_seq_alignments(alig_list)
+
+    sys.stdout = saveout
+    output_fh.close()
+
+
+if __name__ == "__main__":
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    # get all alignments from html
+    alig_list = soup.find_all('div', attrs={'class':'seq_alignment'})
+    
+    output_stockholm(alig_list)
+   
